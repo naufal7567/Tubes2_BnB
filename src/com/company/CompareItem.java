2046620f1e833353b74ebbe37b5d29b345b65d83
naufal7567 <naufal7567@gmail.com>
2022-06-24 23:17:19 +0800

    First Commit

---
+++ src/com/company/CompareItem.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Comparator;
+
+
+public class CompareItem implements Comparator<Item> {
+    public int compare(Item item1, Item item2) {
+        double ratio1 = (double)item1.getRating() / item1.getWeight();
+        double ratio2 = (double)item2.getRating() / item2.getWeight();
+        //sort item in descending order of ratio of rating per weight
+        if (ratio1 > ratio2)
+            return -1;
+        else
+            return 1;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,73 @@
+package com.company;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[])
+    {
+        boolean repeatFlag = true;
+        do {
+
+            try {
+                // Create a File instance
+                File file = new File("item_list.txt");
+
+                // Create a Scanner for the file
+                Scanner inputFile = new Scanner(file);
+                ArrayList<Item> itemList = new ArrayList<>();
+                while (inputFile.hasNext()) {
+                    String name = inputFile.nextLine();
+                    String[] temp = inputFile.nextLine().split(" ");
+                    int weight = Integer.parseInt(temp[0]);
+                    int value = Integer.parseInt(temp[1]);
+                    itemList.add(new Item (name, weight, value, 0));
+                }
+                inputFile.close();
+
+                System.out.print("Kapasitas maksimum truk : 12");
+                int capacity = 12;
+                repeatFlag = false;
+                Item[] item = itemList.toArray(new Item[itemList.size()]);
+                BranchAndBound algorithm = new BranchAndBound(item.length, capacity);
+                double upper = algorithm.SolveKnapsack(item);
+
+
+                System.out.println("\n\nBarang yang tersedia");
+                System.out.println("----------------------------------------------------------------------------------------------");
+                System.out.println("Nama Barang\t\t\t\tBerat\t\tProfit\t\tProfit/Berat\tQuantity");
+                for (int i = 0; i < item.length; i++) {
+                    String name = String.format("%-20s", item[i].getName());
+                    String weight = String.format("%-5d", item[i].getWeight());
+                    String rating = String.format("%-5d", item[i].getRating());
+                    String ratio = String.format("%-8.2f", (double)item[i].getRating() / item[i].getWeight());
+                    System.out.println(name + "\t" + weight + "\t\t" + rating + "\t\t\t" + ratio +
+                            "\t   " + item[i].getQuantity());
+                }
+                System.out.println("----------------------------------------------------------------------------------------------");
+                System.out.print("Barang yang terpilih: | ");
+                int totalWeight = 0;
+                for (int i = 0; i < item.length; i++) {
+                    if (item[i].getQuantity() == 1) {
+                        totalWeight += item[i].getWeight();
+                        System.out.print(item[i].getName() +" | ");
+                    }
+                }
+                System.out.println("\nTotal Berat  : " + totalWeight);
+                System.out.println("Profit maksimal: " + upper);
+            }
+            catch (IOException ex){
+                System.out.println("File-related Error Found: " + ex.getMessage());
+                repeatFlag = false;
+            }
+            catch (NumberFormatException ex) {
+                System.out.println("Invalid Input! Only integer is accepted.");
+            }
+            catch (Exception ex) {
+                System.out.println("Exception Occurs: " + ex.getMessage());
+            }
+
+        }while (repeatFlag);
+
+    }
+}
